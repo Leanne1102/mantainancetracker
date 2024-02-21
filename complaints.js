004deb3c1561da07e8a5a22400e697ecef9ce28c
leanne1102 <leanneagiramukama@gmail.com>
2024-02-21 09:33:00 -1200

    edited my work

--- complaints.js
+++ complaints.js
@@ -4,17 +4,30 @@ function hellen(){
     var mia = {sender:"Leanne",complaint:muhi,status:"old"}
     cherise.push(mia)
     localStorage.setItem("dog",JSON.stringify(cherise))
+    window.location.href = "complaintslogin.html"
     console.log(cherise)
 }
 function restie(){
    var kia = localStorage.getItem("dog")
     console.log(kia)
     var mia = JSON.parse(kia)
+    console.log(mia)
+    var tbody = document.getElementById("usher")
+    var row = tbody.insertRow()
+    var cell = row.insertCell()
+    cell.innerHTML = "Leanne"
+    var cell2 = row.insertCell()
+    cell2.innerHTML ="no seats"
+    var cell3 = row.insertCell()
+    cell3.innerHTML = "11.03.24"
+    var cell4 = row.insertCell()
+    cell4.innerHTML = "new"
+
+
+
+
+
+    
 } 
-for(var leanne=0;leanne<7;leanne++){
-    console.log("my name is ritah")
-}
-for (var r=2;r<23;r++){
-    console.log("i will go to heaven")
-}
+
 
